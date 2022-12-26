5363fb8d29178512df180e3ef52764bdd6f7ab78
LGL <1023197082@qq.com>
2022-12-26 18:25:24 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="app">
+    <i class="toutiao toutiaoshanchu"></i>
+    <router-view />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style lang="less">
+
+</style>
